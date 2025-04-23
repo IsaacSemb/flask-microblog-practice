88504db99ef2d0c1e48e870dc4b3ab7539e9a912
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-23 23:41:38 +0100

    Displaying out to use loops in templates

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,20 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    context = {
+    context1 = {
         "username":"Isaac Ssembuusi",
         "title": "Home Page"
         }
-    return render_template( 'index.html', **context )
+    
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+     
+    return render_template( 'index.html', **context1, posts=posts )
