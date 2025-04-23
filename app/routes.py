356303aadd5e16485d04bdbfc5fa747dfc5c2595
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-23 22:04:29 +0100

    changing the view unction to a template based approach

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"username":"Ssembuusi Isaac"}
-    return f"""
-<html>
-<head>
-    <title>Home Page - Microblog</title>
-</head>
-<body>
-    <h1>Hello, {user['username']}!</h1>
-</body>
-</html>
-
-"""
+    context = {
+        "username":"Isaac Ssembuusi",
+        "title": "Home Page"
+        }
+    return render_template( 'index.html', **context )
