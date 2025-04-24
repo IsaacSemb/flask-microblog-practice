56dea219322ae4aef23191849fcc8ca7c4693d62
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-25 00:34:07 +0100

    create the login form for the login page

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import validators
+
+class LoginForm(FlaskForm):
+    username = StringField( label="", validators=[validators.DataRequired("Username is missing")] )
+    password = PasswordField( label="", validators=[validators.DataRequired("Password is missing")] )
+    remember_me = BooleanField( label="Remember Me" )
+    submit = SubmitField( label="Sign In" )
+    
