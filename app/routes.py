189d3c38eb8cec6930c71d7489da2cf8d965364b
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-25 01:12:27 +0100

    - Adding method to the route for form to be accepted - Adding logic for form accpetance through checking for form validation

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -21,11 +21,21 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-     
+    
     return render_template( 'index.html', **context1, posts=posts )
 
-@app.route('/login')
+@app.route('/login', methods=[ 'GET', 'POST' ])
 def login():
     login_form = LoginForm()
+    
+    if login_form.validate_on_submit():
+        flash( message=f"Login requested for user: {login_form.username.data}, rememeber_me={login_form.remember_me.data}" )
+        
+        print(login_form.username)
+        print(login_form.password)
+        print(login_form.remember_me)
+        
+        return redirect('/index')
+    
     return render_template('login.html', title="Sign In", form=login_form )
     
