f02cbbe42b1293937cb53dd6f0c030d29dff2da7
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-30 08:45:37 +0100

    Initialise sql alchemy and migration in the application initialisation file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
@@ -6,4 +8,7 @@ app = Flask(__name__)
 # pull in configuration settings
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
