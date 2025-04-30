e8dc57600c4ce17a986d5bee71e17f7ae3d986fd
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-30 08:31:05 +0100

    configuring database in configuration file

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    # our apps secret key
     SECRET_KEY = os.environ.get('SECRET_KEY') or "you-will-never-guess"
+    
+    # database configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATATBASE_URL') or \
+        'sqllite:///' + os.path.join(basedir, 'app.db')
+    
