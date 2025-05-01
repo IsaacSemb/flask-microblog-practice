9c4eb8616bda0d2a194f1fdf436880435cb63bfa
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-01 18:30:38 +0100

    bugfix: mapping posts back to the post class from the users

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(db.Model):
     email:so.Mapped[str]  = so.mapped_column( sa.String(120), index=True, unique=True)
     password_hash:so.Mapped[Optional[str]]  = so.mapped_column(sa.String(256))
     
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    
     def __repr__(self):
         return f"<User {self.username}>"
     
@@ -24,3 +26,4 @@ class Post(db.Model):
     
     def __repr__(self):
         return f"<Post {self.body}>"
+    
