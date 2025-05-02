25b80f0c9a870018ee7e2ac317514d48ce93e96e
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-02 11:26:07 +0100

    Implement login logic for a user

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import LoginForm
+
+from flask_login import current_user, login_user
+from app.models import User
+import sqlalchemy as sa
+
 
 
 @app.route('/')
@@ -26,15 +31,33 @@ def index():
 
 @app.route('/login', methods=[ 'GET', 'POST' ])
 def login():
+    
+    # checked if the user is authenticated already
+    # An already logged in user should not go to the login page
+    if current_user.is_authenticated:
+        return  redirect(url_for('index'))
+    
+    # pass the login form to the view
     login_form = LoginForm()
     
     if login_form.validate_on_submit():
-        flash( message=f"Login requested for user: {login_form.username.data}, remember_me={login_form.remember_me.data}" )
         
-        print(login_form.username)
-        print(login_form.password)
-        print(login_form.remember_me)
+        # check for user details from submitted form in the database
+        user = db.session.scalar(
+            sa
+            .select(User)
+            .where(User.username==login_form.username.data)
+        )
         
+        # check password if the user exists
+        if user is None or not user.check_password(login_form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        
+        # if sucessfull if statement wont run
+        login_user(user, remember=login_form.remember_me.data)
+        
+        # send the user to the homepage
         return redirect(url_for('index'))
     
     return render_template('login.html', title="Sign In", form=login_form )
