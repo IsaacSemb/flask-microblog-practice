8f80a5c5653867817a7b8e82289d03d5b2038d4f
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-10 19:01:50 +0100

    Set up file logging system

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask 
 
 
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -56,7 +57,26 @@ if not app.debug: # if application is not in debug mode
             secure=secure
         )
         mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)     
+        app.logger.addHandler(mail_handler)
+
+    # setting file logger
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log',
+        maxBytes = 10240,
+        backupCount = 10
+        )
+    
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 
 
