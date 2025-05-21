62cb5ca7ad9b399b8661351feac27af6c29df8df
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-21 22:57:29 +0100

    Implement functionality to pull posts from all users a user follows

--- app/models.py
+++ app/models.py
@@ -113,6 +113,22 @@ class User(db.Model, UserMixin):
         )
         return db.session.scalar(query)
     
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        
+        query = (
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower))
+            .where(Follower.id==self.id)
+            .order_by(Post.timestamp.desc())
+        )
+        
+        print(query)
+        
+        return query
+    
     # ---------- FOLLOWING FUNCTIONALITY END-----------
     
     
